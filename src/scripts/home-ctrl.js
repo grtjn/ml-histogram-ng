5e9316eb8af72c27383ed58046fd8957dd8dc3c8
Geert Josten <geert.josten@marklogic.com>
2017-02-23 10:33:48 +0100

    Added extra facets for testing mixtures

--- src/scripts/home-ctrl.js
+++ src/scripts/home-ctrl.js
@@ -46,17 +46,23 @@
         if (ctrl.histogramFacets && ctrl.histogramFacets.Period1.facetValues[1].value === '1996') {
           ctrl.histogramFacets = {
             Period1: angular.copy(monthsResponse.facets.Period1),
-            Period2: angular.copy(monthsResponse.facets.Period2)
+            Period2: angular.copy(monthsResponse.facets.Period2),
+            Period3: angular.copy(monthsResponse.facets.Period3),
+            Period4: angular.copy(monthsResponse.facets.Period4)
           };
         } else if (ctrl.histogramFacets && ctrl.histogramFacets.Period1.facetValues[0].value === '2014-01') {
           ctrl.histogramFacets = {
             Period1: angular.copy(daysResponse.facets.Period1),
-            Period2: angular.copy(daysResponse.facets.Period2)
+            Period2: angular.copy(daysResponse.facets.Period2),
+            Period3: angular.copy(daysResponse.facets.Period3),
+            Period4: angular.copy(daysResponse.facets.Period4)
           };
         } else {
           ctrl.histogramFacets = {
             Period1: angular.copy(yearsResponse.facets.Period1),
-            Period2: angular.copy(yearsResponse.facets.Period2)
+            Period2: angular.copy(yearsResponse.facets.Period2),
+            Period3: angular.copy(yearsResponse.facets.Period3),
+            Period4: angular.copy(yearsResponse.facets.Period4)
           };
         }
       });
